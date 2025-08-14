9a681d1c90bd482dfef7f7a548ebb73b71313ac8
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-14 07:47:57 -0300

    estudando sobre assincronismo

---
+++ assincronismo/set-interval.js
@@ -0,0 +1,14 @@
+// setInterval() executa uma função após um intervalo de tempo especificado
+
+let value = 10; 
+
+const interval = setInterval(() => { 
+    console.log(value);
+    value--;
+
+    if(value === 0) {
+        console.log("FELIZ ANO NOVO!");
+        clearInterval(interval);
+    }
+}, 1000);
+
---
+++ assincronismo/set-timeout.js
@@ -0,0 +1,6 @@
+
+// setTimeout() executa uma função após um determinado tempo especificado
+
+setTimeout(() => { 
+    console.log("Hello World!");
+}, 3000) // Tempo em milisegundos
