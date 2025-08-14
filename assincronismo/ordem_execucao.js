2c994552336dbd65f89ec4b2dee86fecfac34a18
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-14 13:19:46 -0300

    entendendo ordem de execução

--- assincronismo/ordem_execucao.js
+++ assincronismo/ordem_execucao.js
@@ -4,3 +4,12 @@ queueMicrotask(() => {
     console.log(2);
 });
 
+setTimeout(() => { 
+    console.log(3);
+}, 1000);
+
+console.log(4);
+
+Promise.resolve(true).then(() => { 
+    console.log(5);
+})
